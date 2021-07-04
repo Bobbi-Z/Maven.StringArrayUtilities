4eb27e83d4977594f5f8a9f36e98f5ede8088f98
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-04 16:41:02 -0400

    removeconsecutiveduplicates attempt 7

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -183,15 +183,16 @@ public class StringArrayUtils<coun> {
 
     public static String[] removeConsecutiveDuplicates(String[] array) {
         String[] newArray = new String[array.length - 1];
-        for (int index = array.length - 1, newIndex = newArray.length -1; index < 0; index--) {
+        for (int index = array.length - 1, newIndex = newArray.length - 1; index < 0; index--) {
             if (array[index] == array[index - 1]) {
                 continue;
             } else {
-                newArray[newIndex--] = array[index];
+                newArray[newIndex] = array[index];
+                newIndex--;
             }
         }
         return newArray;
-
+    }
       /*  String[] arrayNew = new String[array.length - 1];
         int indexOld = 0;
         int indexNew = 0;
