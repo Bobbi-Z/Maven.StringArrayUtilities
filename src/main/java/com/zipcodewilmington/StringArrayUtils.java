92317cfcc62e5ea85b181aa6d15f6b9c69dc6be4
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-03 23:15:27 -0400

    ispangramic attempt 3

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -115,6 +115,8 @@ public class StringArrayUtils<coun> {
                 if (currentChar == currentAlpha) {
                     counter += 1;
 
+                }else {
+                    continue;
                 }
             }
         }
