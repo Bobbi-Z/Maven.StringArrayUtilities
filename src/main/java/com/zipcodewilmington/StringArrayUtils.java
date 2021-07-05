e5c961799ffbcd508e57a662b84d67b5d728c8f4
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-04 20:32:27 -0400

    packconsecutiveduplicate attempt 19

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -241,7 +241,7 @@ public class StringArrayUtils<coun> {
             // currentValue = array[0];
             if (currentValue == (lastValue.substring(0))) {
                lastAndCurrent = currentValue + lastValue;
-              arrayString =  StringUtils.replace(lastValue, lastValue, lastAndCurrent);
+              arrayString =  StringUtils.replace(arrayString, lastValue, lastAndCurrent, lastValue.length()-1);
             } else {
                 arrayString += currentValue + " ";
                 lastValue = currentValue;
